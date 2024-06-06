419473d1bf815032584747011ea9d6162a5457a6
N2H4 <n.nawrocki@onet.pl>
2024-06-06 21:38:24 +0200

    more book gui

--- src/main/java/com/N2H4/arcanerefraction/client/gui/GuideContent.java
+++ src/main/java/com/N2H4/arcanerefraction/client/gui/GuideContent.java
@@ -5,13 +5,14 @@ import static com.N2H4.arcanerefraction.ArcaneRefractionMod.MODID;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import net.minecraft.client.Minecraft;
@@ -24,18 +25,31 @@ public class GuideContent
     private ResourceManager manager=Minecraft.getInstance().getResourceManager();
     private static final ResourceLocation location = new ResourceLocation(MODID, "ar_guide_content.xml");
 
-    private String tmp="";
-
-    private List<List<String>> intro;
+    private Dictionary<String, List<List<String>>> content;
 
     public GuideContent()
     {
-        intro=new ArrayList<>();
+        content = new Hashtable<>();
+        content.put("introLeft", new ArrayList<>());
+        content.put("introRight", new ArrayList<>());
+        content.put("basicsLeft", new ArrayList<>());
+        content.put("basicsRight", new ArrayList<>());
+        content.put("sizesLeft", new ArrayList<>());
+        content.put("sizesRight", new ArrayList<>());
+        content.put("effectsLeft", new ArrayList<>());
+        content.put("effectsRight", new ArrayList<>());
+        content.put("transformationsLeft", new ArrayList<>());
+        content.put("transformationsRight", new ArrayList<>());
     }
 
-    public List<List<String>> getStartChapter()
+    public List<String> getChapterPage(String chapter, int page)
     {
-        return intro;
+        return content.get(chapter).get(page);
+    }
+
+    public int getChapterPages(String chapter)
+    {
+        return content.get(chapter).size();
     }
 
     public void loadGuide()
@@ -80,26 +94,102 @@ public class GuideContent
 
         doc.getDocumentElement().normalize();
 
-        tmp=doc.getElementsByTagName("line").item(0).getTextContent();
-
         //parse intro
+        List<String> linesList;
+        NodeList lines;
         NodeList pages = doc.getElementsByTagName("intro").item(0).getChildNodes();
-        for(int i=1;i<pages.getLength()-1;i++)
+        for(int i=1;i<pages.getLength()-1;i+=2)
         {
-            List<String> linesList=new ArrayList<>();
-            NodeList lines=pages.item(i).getChildNodes().item(1).getChildNodes();
-            System.out.println(lines.getLength());
+            linesList=new ArrayList<>();
+            lines=pages.item(i).getChildNodes().item(1).getChildNodes();
             for(int j=1;j<lines.getLength()-1;j+=2)
             {
                 linesList.add(lines.item(j).getTextContent());
             }
-            this.intro.add(linesList);
+            this.content.get("introLeft").add(linesList);
+            this.content.get("introRight").add(new ArrayList<>());
         }
-    }
 
-    public String getTmp()
-    {
-        return tmp;
+        //parse basics
+        pages = doc.getElementsByTagName("basics").item(0).getChildNodes();
+        for(int i=1;i<pages.getLength()-1;i+=2)
+        {
+            linesList=new ArrayList<>();
+            lines=pages.item(i).getChildNodes().item(1).getChildNodes();
+            for(int j=1;j<lines.getLength()-1;j+=2)
+            {
+                linesList.add(lines.item(j).getTextContent());
+            }
+            this.content.get("basicsLeft").add(linesList);
+            linesList=new ArrayList<>();
+            lines=pages.item(i).getChildNodes().item(3).getChildNodes();
+            for(int j=1;j<lines.getLength()-1;j+=2)
+            {
+                linesList.add(lines.item(j).getTextContent());
+            }
+            this.content.get("basicsRight").add(linesList);
+        }
+
+        //parse sizes
+        pages = doc.getElementsByTagName("sizes").item(0).getChildNodes();
+        for(int i=1;i<pages.getLength()-1;i+=2)
+        {
+            linesList=new ArrayList<>();
+            lines=pages.item(i).getChildNodes().item(1).getChildNodes();
+            for(int j=1;j<lines.getLength()-1;j+=2)
+            {
+                linesList.add(lines.item(j).getTextContent());
+            }
+            this.content.get("sizesLeft").add(linesList);
+            linesList=new ArrayList<>();
+            lines=pages.item(i).getChildNodes().item(3).getChildNodes();
+            for(int j=1;j<lines.getLength()-1;j+=2)
+            {
+                linesList.add(lines.item(j).getTextContent());
+            }
+            this.content.get("sizesRight").add(linesList);
+        }
+
+        //parse effects
+        pages = doc.getElementsByTagName("effects").item(0).getChildNodes();
+        for(int i=1;i<pages.getLength()-1;i+=2)
+        {
+            linesList=new ArrayList<>();
+            lines=pages.item(i).getChildNodes().item(1).getChildNodes();
+            for(int j=1;j<lines.getLength()-1;j+=2)
+            {
+                linesList.add(lines.item(j).getTextContent());
+            }
+            this.content.get("effectsLeft").add(linesList);
+            linesList=new ArrayList<>();
+            lines=pages.item(i).getChildNodes().item(3).getChildNodes();
+            for(int j=1;j<lines.getLength()-1;j+=2)
+            {
+                linesList.add(lines.item(j).getTextContent());
+            }
+            this.content.get("effectsRight").add(linesList);
+        }
+
+        //parse transformations
+        pages = doc.getElementsByTagName("transformations").item(0).getChildNodes();
+        for(int i=1;i<pages.getLength()-1;i+=2)
+        {
+            linesList=new ArrayList<>();
+            lines=pages.item(i).getChildNodes().item(1).getChildNodes();
+            for(int j=1;j<lines.getLength()-1;j+=2)
+            {
+                linesList.add(lines.item(j).getTextContent());
+            }
+            this.content.get("transformationsLeft").add(linesList);
+            linesList=new ArrayList<>();
+            lines=pages.item(i).getChildNodes().item(3).getChildNodes();
+            for(int j=1;j<lines.getLength()-1;j+=2)
+            {
+                linesList.add(lines.item(j).getTextContent());
+            }
+            this.content.get("transformationsRight").add(linesList);
+        }
+
     }
     
 }
--- src/main/java/com/N2H4/arcanerefraction/client/screen/ARGuideScreen.java
+++ src/main/java/com/N2H4/arcanerefraction/client/screen/ARGuideScreen.java
@@ -1,14 +1,15 @@
 package com.N2H4.arcanerefraction.client.screen;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.PageButton;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.Mth;
 
 import static com.N2H4.arcanerefraction.ArcaneRefractionMod.MODID;
+
+import java.util.List;
+
 import com.N2H4.arcanerefraction.client.gui.ChapterButton;
 import com.N2H4.arcanerefraction.client.gui.GuideContent;
 
@@ -19,17 +20,24 @@ public class ARGuideScreen extends Screen
     private static final ResourceLocation TEXTURE = new ResourceLocation(MODID, "textures/gui/ar_guide_bg4.png");
     private PageButton forwardButton;
     private PageButton backButton;
+    private PageButton startButton;
     private ChapterButton basicsButton;
     private ChapterButton lensSizesButton;
     private ChapterButton effectsButton;
     private ChapterButton transformationButton;
     private GuideContent content;
+    private String currenChapter;
+    private int currentPage;
+    private boolean firstTimeOpened;
 
     public ARGuideScreen()
     {
         super(CommonComponents.EMPTY);
+        currenChapter="intro";
+        currentPage=0;
         content=new GuideContent();
         content.loadGuide();
+        firstTimeOpened=true;
     }
 
 
@@ -42,37 +50,69 @@ public class ARGuideScreen extends Screen
     @Override
     public void render(@NotNull GuiGraphics pGuiGraphics, int pMouseX, int pMouseY, float pPartialTick) {
         super.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
-        drawStartPage(pGuiGraphics);
+        drawPage(pGuiGraphics);
     }
 
     private void setStartPage()
     {
+        currenChapter="intro";
+        currentPage=0;
         this.forwardButton.visible=false;
         this.backButton.visible=false;
+        this.startButton.visible=false;
         this.basicsButton.visible=true;
         this.lensSizesButton.visible=true;
         this.effectsButton.visible=true;
         this.transformationButton.visible=true;
     }
 
-    private void drawStartPage(GuiGraphics pGuiGraphics)
-    {  
-        int lineOffset=57;
-        for (String line : content.getStartChapter().get(0)) 
+    private void setChapterPage(String chapter)
+    {
+        currenChapter=chapter;
+        if(getMaxPages()>1)
         {
-            drawSmallString(pGuiGraphics, line,lineOffset);
+            this.forwardButton.visible=true;
+        }
+        this.startButton.visible=true;
+        this.basicsButton.visible=false;
+        this.lensSizesButton.visible=false;
+        this.effectsButton.visible=false;
+        this.transformationButton.visible=false;
+    }
+
+
+    private int getMaxPages()
+    {
+        return content.getChapterPages(currenChapter+"Left");
+    }
+
+    private void drawPage(GuiGraphics pGuiGraphics)
+    {  
+        //draw left
+        int lineOffset=105;
+        List<String> lines=content.getChapterPage(currenChapter+"Left", currentPage);
+        for (String line : lines)
+        {
+            drawSmallString(pGuiGraphics, line,198, lineOffset);
+            lineOffset+=10;
+        }
+        //draw right
+        lineOffset=105;
+        lines=content.getChapterPage(currenChapter+"Right", currentPage);
+        for (String line : lines)
+        {
+            drawSmallString(pGuiGraphics, line,-20, lineOffset);
             lineOffset+=10;
         }
     }
 
-    private void drawSmallString(GuiGraphics pGuiGraphics, String line,int lineOffset)
+    private void drawSmallString(GuiGraphics pGuiGraphics, String line,int position, int lineOffset)
     {
         var pose = pGuiGraphics.pose();
         pose.pushPose();
         {
-            pose.translate(40, 15, 0);
-            pose.scale(0.75f, 0.75f, 1f);
-            pGuiGraphics.drawString(minecraft.font, line, ((this.width-256)/2)+10, lineOffset, 1184274 ,false);
+            pose.scale(0.6f, 0.6f, 0.6f);
+            pGuiGraphics.drawString(minecraft.font, line, ((this.width*0.5f)/0.6f)-position, lineOffset, 1184274 ,false);
         }
         pose.popPose();
     }
@@ -81,29 +121,89 @@ public class ARGuideScreen extends Screen
         int i = (this.width - 256) / 2;
         this.forwardButton = this.addRenderableWidget(new PageButton(i + 225, 182, true, p_98297_ -> this.pageForward(), false));
         this.backButton = this.addRenderableWidget(new PageButton(i+1, 182, false, p_98287_ -> this.pageBack(), false));
+        this.startButton = this.addRenderableWidget(new PageButton(i+1, 50, false, p_98287_ -> this.pageStart(), false));
     }
 
     protected void createStartControls() {
-        this.basicsButton=this.addRenderableWidget(new ChapterButton((this.width/2)+17, 65, true, p_98297_ -> this.pageForward(),"Basics"));
-        this.lensSizesButton=this.addRenderableWidget(new ChapterButton((this.width/2)+17, 82, true, p_98297_ -> this.pageForward(),"Lens Sizes"));
-        this.effectsButton=this.addRenderableWidget(new ChapterButton((this.width/2)+17, 99, true, p_98297_ -> this.pageForward(),"Effects"));
-        this.transformationButton=this.addRenderableWidget(new ChapterButton((this.width/2)+17, 116, true, p_98297_ -> this.pageForward(),"Transformations"));
+        this.basicsButton=this.addRenderableWidget(new ChapterButton((this.width/2)+17, 65, true, p_98297_ -> this.setChapterPage("basics"),"Basics"));
+        this.lensSizesButton=this.addRenderableWidget(new ChapterButton((this.width/2)+17, 82, true, p_98297_ -> this.setChapterPage("sizes"),"Lens Sizes"));
+        this.effectsButton=this.addRenderableWidget(new ChapterButton((this.width/2)+17, 99, true, p_98297_ -> this.setChapterPage("effects"),"Effects"));
+        this.transformationButton=this.addRenderableWidget(new ChapterButton((this.width/2)+17, 116, true, p_98297_ -> this.setChapterPage("transformations"),"Transformations"));
     }
 
     @Override
     protected void init() {
         this.createPageControlButtons();
         this.createStartControls();
-        this.setStartPage();
+        if(firstTimeOpened)
+        {
+            firstTimeOpened=false;
+            this.setStartPage();
+        }
+    }
+
+    @Override
+    protected void rebuildWidgets() {
+        super.rebuildWidgets();
+        if(currenChapter!="intro")
+        {
+            if(getMaxPages()>1 && currentPage!=getMaxPages()-1)
+            {
+                this.forwardButton.visible=true;
+            }
+            else
+            {
+                this.forwardButton.visible=false;
+            }
+            this.startButton.visible=true;
+            this.basicsButton.visible=false;
+            this.lensSizesButton.visible=false;
+            this.effectsButton.visible=false;
+            this.transformationButton.visible=false;
+        }
+        else
+        {
+            this.forwardButton.visible=false;
+            this.startButton.visible=false;
+            this.backButton.visible=false;
+            this.basicsButton.visible=true;
+            this.lensSizesButton.visible=true;
+            this.effectsButton.visible=true;
+            this.transformationButton.visible=true;
+        }
     }
 
     private void pageBack() {
-        System.out.println("left");
+        this.forwardButton.visible=true;
+        if(currentPage>0)
+        {
+            currentPage--;
+        }
+        if(currentPage==0)
+        {
+            this.backButton.visible=false;
+        }
+    }
+
+    private void pageStart() 
+    {
+        currenChapter="intro";
+        currentPage=0;
+        setStartPage();
     }
 
 
-    private void pageForward() {
-        System.out.println("right");
+    private void pageForward() 
+    {
+        this.backButton.visible=true;
+        if(currentPage<getMaxPages()-1)
+        {
+            currentPage++;
+        }
+        if(currentPage==getMaxPages()-1)
+        {
+            this.forwardButton.visible=false;
+        }
     }
 
     @Override
